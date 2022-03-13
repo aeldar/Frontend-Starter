8e28fa51fb64f9df2e993ee9cbfb02577a8cc8c8
aeldar <eldar.aliyev8@gmail.com>
2022-03-13 18:53:18 +0500

    chore: add material

--- apps/starter-shop/src/app/app.module.ts
+++ apps/starter-shop/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
