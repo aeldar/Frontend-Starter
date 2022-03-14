f2a04e4382756f79d658b71e0658b05e235643ca
aeldar <eldar.aliyev8@gmail.com>
2022-03-14 21:28:15 +0500

    extract shell to lib

--- apps/starter-shop/src/app/app.module.ts
+++ apps/starter-shop/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { ShellComponentModule } from './shell/shell.component';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ENV } from '@starter/env';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShellComponentModule } from '@starter/ui/shell';
 
 @NgModule({
   declarations: [AppComponent],
