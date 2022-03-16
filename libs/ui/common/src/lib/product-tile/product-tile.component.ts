3518685933d69e677f7a6743d916ebbceec3a87d
aeldar <eldar.aliyev8@gmail.com>
2022-03-16 16:37:22 +0500

    add stubs for cart and edit button

--- libs/ui/common/src/lib/product-tile/product-tile.component.ts
+++ libs/ui/common/src/lib/product-tile/product-tile.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '@starter/model';
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatTooltipModule} from "@angular/material/tooltip";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'starter-product-tile',
@@ -16,6 +17,21 @@ export class ProductTileComponent {
   @Input() product: Product | null = null;
   @Input() isAdmin = false;
   @Input() isAuthenticated = false;
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  onAddToCart() {
+    this.notYetImplemented();
+  }
+
+  onEdit() {
+    this.notYetImplemented();
+  }
+
+  private notYetImplemented() {
+    console.log('TODO implement');
+    this.snackBar.open('Sorry, not yet implemented', undefined, { duration: 2500 });
+  }
 }
 
 @NgModule({
