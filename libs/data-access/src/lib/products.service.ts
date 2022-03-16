7ff2a624136f235f5b7a95ce9b1bf7890c19cf3f
aeldar <eldar.aliyev8@gmail.com>
2022-03-16 12:04:18 +0500

    improve products

--- libs/data-access/src/lib/products.service.ts
+++ libs/data-access/src/lib/products.service.ts
@@ -3,17 +3,15 @@ import { Env, ENV } from '@starter/env';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '@starter/model';
 
-const API_PATH = 'products';
-
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  private readonly uri: string = `${this.env.API_URL}/${API_PATH}`;
+  private readonly uri: string = `${this.env.API_URL}/${this.env.PRODUCTS_API_PATH}`;
 
   constructor(@Inject(ENV) private env: Env, private http: HttpClient) {}
 
-  getProducts(page: number, limit: number = this.env.PRODUCT_DEFAULT_LIMIT) {
+  getProducts(page: number = 0, limit: number = this.env.PRODUCTS_DEFAULT_LIMIT) {
     return this.http.get<Product[]>(this.uri);
   }
 }
